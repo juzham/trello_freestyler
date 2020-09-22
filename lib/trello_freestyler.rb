9512e36e3557c1a18105373579d24dd571275bfd
Justin Hamman <justin.hamman@rea-group.com>
2020-09-23 06:47:00 +1000

    add docker

--- lib/trello_freestyler.rb
+++ lib/trello_freestyler.rb
@@ -5,24 +5,28 @@ require_relative 'trello_freestyler/cli'
 require_relative 'trello_freestyler/main'
 require 'tty-spinner'
 
-if ARGV.length.positive?
-  spinners = TTY::Spinner::Multi.new("[:spinner] Trello Freestyler")
-  sp1 = spinners.register "[:spinner] Validate input options"
-  sp1.auto_spin
+module TrelloFreestyler
+  def self.run
+    if ARGV.length.positive?
+      spinners = TTY::Spinner::Multi.new("[:spinner] Trello Freestyler")
+      sp1 = spinners.register "[:spinner] Validate input options"
+      sp1.auto_spin
 
-  options = TrelloFreestyler::Cli.parse(ARGV)
+      options = TrelloFreestyler::Cli.parse(ARGV)
 
-  sp1.success
+      sp1.success
 
-  sp2 = spinners.register "[:spinner] Download and clean trello data"
-  sp3 = spinners.register "[:spinner] Output data to: '#{options.output}'"
-  sp2.auto_spin
-  sp3.auto_spin
+      sp2 = spinners.register "[:spinner] Download and clean trello data"
+      sp3 = spinners.register "[:spinner] Output data to: '#{options.output}'"
+      sp2.auto_spin
+      sp3.auto_spin
 
-  TrelloFreestyler::Main.dump(options)
+      TrelloFreestyler::Main.dump(options)
 
-  sp2.success
-  sp3.success
-else
-  TrelloFreestyler::Cli.parse %w[--help]
+      sp2.success
+      sp3.success
+    else
+      TrelloFreestyler::Cli.parse %w[--help]
+    end
+  end
 end
